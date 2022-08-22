11d632018eef99a655e52bfec6fdfd4302bd0c52
Barnaan <barnaan@gmail.com>
2022-08-22 22:41:24 +0300

    added templates and static folders

--- Bus_booking/settings.py
+++ Bus_booking/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booker.apps.BookerConfig',
+    'customer.apps.CustomerConfig',
+    'bus_admin.apps.BusAdminConfig',
+    'system_admin.apps.SystemAdminConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,9 @@ ROOT_URLCONF = 'Bus_booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+             BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ bus_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BusAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bus_admin'
