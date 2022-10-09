ebc59ee1ecf4d315bd7368acaa3d4179cfb3efaf
Barnaan2 <barnaantekalign@gmail.com>
2022-10-09 07:52:21 -0700

    updating the index ui

--- customer/views.py
+++ customer/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from account.models import User
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm
 from booker.models import SubRoute, Route
 from booking.models import Booking
+from system_admin.models import BusBrand
 ### please do not import all classes from .models because there may be error while login
 
 from django.contrib.auth import authenticate, login, logout
@@ -13,37 +14,40 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
-    q=request.GET.get('q') 
-    if request.GET.get('q') != None: 
+    start =request.GET.get('x')
+    destination = request.GET.get('y')
+    date = request.GET.get('z')
+    if request.GET.get('start') != None:
       routes=Route.objects.filter(
-    
+
         Q(first_city__icontains=q) |
         Q(second_city__icontains=q) )
     else:
         routes = Route.objects.all()
-    context={'routes':routes}
+    buses = BusBrand.objects.all();
+    context={'routes':routes,'buses':buses}
     return render(request, 'customer/home.html', context)
 
 
 def subRoute(request, pk):
     route=Route.objects.get(id=pk)
     sub_routes=route.subroute_set.all()
-   
+
     context={'route':route, 'sub_routes':sub_routes}
     return render(request, 'customer/sub_route.html', context)
 
-    
+
 def myBooking(request, pk):
     user=User.objects.get(id=pk)
     bookings=user.booking_set.filter().order_by('-created')
-    
+
     context={'bookings':bookings}
     return render(request, 'customer/my_booking.html', context)
 
 # def pay(request, pk):
 #     booking=Booking.objects.get(id=pk)
 #     payment_infos=booking.route.paymentinformantion_set.all()
-    
+
 #     if request.method == 'POST':
 #         finish_payment =FinishPayment.objects.create(
 #         booking=booking,
@@ -52,7 +56,7 @@ def myBooking(request, pk):
 #         transaction_id=request.POST['transaction_id'],
 #         )
 #         return redirect ('my-booking', pk=request.user.id)
-    
+
 #     context={'booking':booking, 'payment_infos':payment_infos}
 #     return render(request, 'customer/pay.html', context)
 
@@ -64,17 +68,17 @@ def myBooking(request, pk):
 
 #         try:
 #              user=User.objects.get(username=username)
-           
+
 #         except:
 #             messages.error(request, 'Sorry! User does not exist.')
 #         user=authenticate(request, username=username, password=password)
-        
+
 #         if user is not None:
-           
+
 #             login(request, user)
 #             return redirect('home')
 #         else :
-#           messages.error(request, 'Sorry! username or password does not exist.')  
+#           messages.error(request, 'Sorry! username or password does not exist.')
 #     context={'page':page}
 #     return render(request, 'customer/login_register.html', context)
 
@@ -87,7 +91,7 @@ def myBooking(request, pk):
 # def registerPage(request):
 #     page='register'
 #     form=UserCreationForm()
-    
+
 #     if request.method =='POST':
 #         form = UserCreationForm(request.POST)
 #         if form.is_valid():
