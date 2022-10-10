03da6f0e6f418558e889f08c11f5023b10917dd2
Barnaan2 <barnaantekalign@gmail.com>
2022-10-09 18:30:34 -0700

    more endpoints added to the api

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-    path('', views.index, name="api_index"),
-    path('bus-brand/', views.bus_brand, name="api_index"),
-#     path('manage_subroute/',views.manage_subroute, name="manage_subroute"),
-#    path('add_subroute/',views.add_subroute, name="add_subroute"),
+    path('', views.index),
+    path('bus-brand/', views.bus_brand),
+    path('route/',views.route),
+ path('sub_route/',views.sub_route),
 #    path('update_subroute/<str:id>/',views.update_subroute, name="update_subroute"),
     # path('booking-request/<str:pk>/', views.bookingRequest, name="booking-request"),
     # path('paid-unpaid/<str:pk>/', views.finishPaymentStatus, name="paid-unpaid"),
--- api/views.py
+++ api/views.py
@@ -21,3 +21,20 @@ def bus_brand(request):
    bus_brand = BusBrand.objects.all()
    serialized_bus_brand = BusBrandSerail(bus_brand,many=True)
    return Response(serialized_bus_brand.data)
+
+@api_view(['GET'])
+def route(request):
+   routes= {
+    'is this for routes?': 'yes its. now this si just place holder '
+   }
+   
+   return Response(routes)
+
+@api_view(['GET'])
+def sub_route(request):
+   sub_routes= {
+    'is this for sub-rotues?': 'yes its. now this si just place holder '
+   }
+   
+   return Response(sub_routes)
+
